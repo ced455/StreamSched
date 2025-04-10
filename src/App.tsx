285baacccbe38dd2ad33be687176b32c3758a40d
chauvieux <cedric.hauvieux@zol.fr>
2025-04-10 10:54:55 +0200

    fix(App): 🐛 update redirect URI to include hash fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { Home } from './components/Home';
 import { Auth } from './components/auth/Auth';
 import { CalendarView } from './features/schedule/components/CalendarView';
